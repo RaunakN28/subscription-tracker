a727ccb59ff607a967792e6fefbd23b01941c25d
RaunakN28 <raunaknegi02@gmail.com>
2025-12-04 20:34:59 +0530

    Add MongoDB connection and user/subscription models
    - Implemented MongoDB connection logic in a new database module.
    - Updated environment configuration to include DB_URI.
    - Added user and subscription models with validation and default values.
    - Updated app.js to establish database connection on server start.
    - Included MongoDB and Mongoose as dependencies in package.json.

---
+++ models/subscription.model.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    price: {
+        type: Number,
+        required: [true, 'Subscription price is required'],
+        min: [0, 'Price must be greater than 0']
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBP', 'INR'],
+        deafult: 'USD'
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'canceled', 'expired'],
+        default: 'active'
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: (value) => value <= new Date(),
+            message: 'Start date cannot be in the past',
+        }
+    },
+    renewalDate: {
+        type: Date,
+        validate: {
+            validator: function (value) {
+                return value > this.startDate;
+            },
+            message: 'Renewal date must be after the start date',
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    }
+}, { timestamps: true });
+
+subscriptionSchema.pre('save',function (next) {
+    if (!this.renewalDate) {
+        const renewalPeriods = {
+            daily: 1,
+            weekly: 7,
+            monthly: 30,
+            yearly: 365
+        };
+
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalData.getDate() + renewalPeriods[this.frequency]);
+    }
+
+    // Auto-update the status if renewal date has passed
+    if (this.renewealData < new Date()) {
+        this.status = 'expired';
+    }
+
+    next();
+});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+export default Subscription;
